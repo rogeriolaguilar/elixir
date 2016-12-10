f49bc5082613405dbfed0368b66789e30ddac05d
Rogério L Aguilar <rogerio.l.aguilar@gmail.com>
2016-12-10 13:06:18 -0200

    max(list)

--- listmap.exs
+++ listmap.exs
@@ -4,9 +4,9 @@ defmodule Listmap do
     Enum.map(list, &(&1 > threshold))
     |> Enum.map(fn true -> 1; false -> 0 end) 
   end
-end
 
-# Running:
-Listmap.binary([1,6,3,8,2,0], 5)
-|> inspect
-|> IO.puts
+  # maximum value of list, like Enum.max
+  def max([x]), do: x
+  def max([head|tail]), do: max(head, max(tail)) # max(number, number) is native in Elixir
+   
+end
