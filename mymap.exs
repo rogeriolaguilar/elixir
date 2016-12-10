5cecb6fe56555a5bfd62f13a350a8447e75f1e4c
Rogério L Aguilar <rogerio.l.aguilar@gmail.com>
2016-12-10 17:57:12 -0200

    changing map values using patther matching

--- mymap.exs
+++ mymap.exs
@@ -4,6 +4,10 @@ defmodule MyMap do
   def tall_people(people) do
     for person = %{ height: height } <- people, height >= @tall_people_height, do: person  
   end
+
+  def call_mister_the_tall_people(people) do
+    for person = %{ name: name} <- tall_people(people), true, do: %{ person|name: "Mr #{name}" }
+  end
 end
 
 #Run
@@ -15,6 +19,11 @@ people = [
   %{ name: "Rogério", last_name: "Aguilar", height: 1.75 }
  ]
 
+puts_result = fn(result, name) -> IO.puts "#{name}: #{inspect result}" end
+
 
 MyMap.tall_people(people)
-|> IO.inspect
+|> puts_result.("tall people")
+
+MyMap.call_mister_the_tall_people(people)
+|> puts_result.( "Mr")
