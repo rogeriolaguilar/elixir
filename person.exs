c32318f37d82674f69d689e1327d94d15eed8211
Rogerio L Aguilar <rogeriolaguilar@gmail.com>
2016-12-10 20:44:04 -0200

    strutc Person

---
+++ person.exs
@@ -0,0 +1,11 @@
+defmodule Person do
+  
+  defstruct name: "", last_name: "", height: 0, subscribed: true
+
+  def subscribed?( person = %Person{}), do: person.subscribed
+  def valid?( person = %Person{}), do: (String.length person.name) > 1 && (String.length person.last_name) > 0
+end
+
+# iex person.exs
+# Person.valid? %Person{name: "Rogério"} # false
+# Person.valid? %Person{name: "Rogério", last_name: "L"} # true
