7342acdd0dd364f6061217bd0cc03f61aacd14e8
Rogério Libarino Aguilar <rogerio.l.aguilar@gmail.com>
2019-09-29 15:29:54 -0300

    communication between process

--- pmap.exs
+++ pmap.exs
@@ -1,9 +1,13 @@
-defmodule ​ Parallel ​ do
+defmodule ParallelMap do
+  def pmap(collection, func) do
+    collection
+    |> Enum.map(&(Task.async( fn -> func.(&1) end)))
+    |> Enum.map(&(Task.await(&1)))
+  end
+end
 
- ​ ​ ​ def ​ pmap(collection, func) ​ do
-      collection ​ 
-      |> Enum.map(&(Task.async(​ fn ​ -> func.(&1) ​ end ​)))
-      |> Enum.map(&Task.await/1)
-    end 
- end
- 
+# a function the let 1 second to run
+func = fn(a) -> Process.sleep(1_000); a+1 end
+
+:timer.tc(ParallelMap, :pmap,  [1..5, func]) # run in 1 second
+:timer.tc(Enum, :map, [1..5, func]) # run in 5 seconds
